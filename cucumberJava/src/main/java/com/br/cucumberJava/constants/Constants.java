0ad15d26a678b2cbb21af260486dfec2edc28951
Welington Junio Rezende Mateus <wj.mateus12@hotmail.com>
2023-03-07 17:09:19 -0300

    Create Constants.java
    criando constants

---
+++ cucumberJava/src/main/java/com/br/cucumberJava/constants/Constants.java
@@ -0,0 +1,9 @@
+package com.br.cucumberJava.constants;
+
+public class Constants {
+	public static final String ERROR_DESCONHECIDO = "** Erro desconhecido **";
+	public static final String MSG_ERROR = "Erro ao processar retorno do caso de teste do cenário: ";
+	public static final String MSG_SUCESSO = "URL gerado com sucesso para o cenário: ";
+	
+	public static final String HOST_CRIAR_CONSULTAR_CEP = "http://localhost:8080/api/v1";
+}
